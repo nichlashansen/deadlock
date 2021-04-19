532cb8598266fbf334a1898d3d7a37984d687528
Morten Hansen <mnbh26515@edu.ucl.dk>
2021-04-19 10:00:06 +0200

    Add project files.

---
+++ deadlock/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deadlock
+{
+    public class Account
+    {
+        public int OneNumber { get; set; } = 2;
+        public int SecondNumber { get; set; } = 2;
+        private decimal balance;
+
+        public Account(decimal initialBalance) => balance = initialBalance;
+
+        public decimal Debit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "The debit amount cannot be negative.");
+            }
+
+            decimal appliedAmount = 0;
+           
+            {
+                if (balance >= amount)
+                {
+                    Program.WhatToDo();
+                    balance -= amount;
+                    appliedAmount = amount;
+                }
+            }
+            return appliedAmount;
+        }
+
+        public void Credit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "The credit amount cannot be negative.");
+            }
+
+           
+            {
+                balance += amount;
+            }
+        }
+
+        public decimal GetBalance()
+        {
+            
+            {
+                return balance;
+            }
+        }
+    }
+}
---
+++ deadlock/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace deadlock
+{
+    class Program
+    {
+        public static void WhatToDo()
+        {
+            Thread thread = new Thread(WhatToDo);
+            Thread threadss = new Thread(WhatToDo);
+            Thread threae = new Thread(WhatToDo);
+
+            thread.Start();
+            threadss.Start();
+            threae.Start();
+
+            var y = 4;
+
+            
+            for (int i = 0; i < 10000; i++)
+            {
+                var rna = new Random();
+                Console.WriteLine($"Thread is working {rna.Next(2343,934798874)}");
+                Account ac = new Account(y);
+
+            }
+        }
+
+
+        class AccountTest
+        {
+
+            static async Task Main()
+            {
+                var account = new Account(1000);
+                var tasks = new Task[100];
+                for (int i = 0; i < tasks.Length; i++)
+                {
+                    tasks[i] = Task.Run(() => Update(account));
+                }
+                await Task.WhenAll(tasks);
+                Console.WriteLine($"Account's balance is {account.GetBalance()}");
+                // Output:
+                // Account's balance is 2000
+            }
+
+            static void Update(Account account)
+            {
+                decimal[] amounts = { 0, 2, -3, 6, -2, -1, 8, -5, 11, -6 };
+                foreach (var amount in amounts)
+                {
+                    if (amount >= 0)
+                    {
+                        account.Credit(amount);
+                    }
+                    else
+                    {
+                        account.Debit(Math.Abs(amount));
+                    }
+                }
+            }
+        }
+    }
+}
+
+
